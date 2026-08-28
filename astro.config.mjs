b9e897279701ba2dc190de5c8c15e9d674dae644
Redwan Alloush <redwaniscool@gmail.com>
2026-08-28 15:47:07 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://RedWn.github.io',
-  base: '/MecroTube',
+  base: process.env.VERCEL ? '/' : '/MecroTube',
   output: 'server',
   adapter: vercel({
     imageService: true,
